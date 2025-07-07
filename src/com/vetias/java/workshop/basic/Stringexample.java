6529f344b532d7a52eade5379233beb77857552b
kiruthikakavitha <kiruthikas24cse@vetias.ac.in>
2025-07-07 10:26:54 +0530

    Stringexample

---
+++ src/com/vetias/java/workshop/basic/Stringexample.java
@@ -0,0 +1,16 @@
+package com.vetias.java.workshop.basic;
+
+public class Stringexample {
+    public static void main(String[] args) {
+        String firstname="kiruthika";
+        String lastname="kiru";
+        String Name=firstname.concat(lastname);
+        System.out.println(Name);
+        String finalname=Name.toUpperCase();
+        System.out.println(finalname);
+    
+   
+    }
+
+
+}
