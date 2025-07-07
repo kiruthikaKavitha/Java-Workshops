8296f785dfd7e073847d8a429dd983bd02e9e0bf
kiruthikakavitha <kiruthikas24cse@vetias.ac.in>
2025-07-07 10:47:53 +0530

    count

--- src/com/vetias/java/workshop/basic/Count.java
+++ src/com/vetias/java/workshop/basic/Count.java
@@ -4,8 +4,8 @@ public class Count {
     public static void main(String[] args) {
         String sentence="this program show how can we split a String into multiple Strings";
         String[] words = sentence.split(" ");
-        int num=words.length;
-        System.out.println(num);
+        int Num=words.length;
+        System.out.println(Num);
            
 
         
