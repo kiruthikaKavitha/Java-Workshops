9d9917547a67d37942e4840f39983986c4cf8c37
kiruthikakavitha <kiruthikas24cse@vetias.ac.in>
2025-07-08 10:00:51 +0530

    calculating our age

---
+++ src/com/vetias/java/workshop/basic/Agecalculator.java
@@ -0,0 +1,17 @@
+package com.vetias.java.workshop.basic;
+
+import java.util.Scanner;
+
+public class Agecalculator {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int currentYear =sc.nextInt();
+        int birthYear = sc.nextInt();
+        int age = currentYear-birthYear;
+        System.out.println("Your age is: " + age);
+}
+
+}
+
+
+
